7d4ee5717c7b2b1c0ecaf8eb73e3dda8498aee79
fernando <ferpb1999@gmail.com>
2020-09-08 16:50:08 +0200

    Corregir cálculo de mu y tau

--- gillespie.py
+++ gillespie.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 import random
+import pdb
 
 
 # Entrada del algoritmo
@@ -55,18 +56,68 @@ species_usage = update_matrix
 species_usage[species_usage > 0] = 0
 species_usage = -species_usage
 
+
+def calculate_a(propensity_functions, species_concentration, species_usage):
+    # Calculamos las a_i
+    a = species_usage * species_concentration
+    # No se si hay que sumar multiplicar
+    # a = np.sum(a, axis=1)
+    a[a == 0] = 1
+    a = np.prod(a, axis=1)
+    a = a * propensity_functions
+
+    a0 = np.sum(a)
+
+    return a, a0
+
+
 # La probabilidad de elección de cada reacción es proporcional a la concentración
 # de sus reactivos en la mezcla
 def next_reaction(propensity_functions, species_concentration, species_usage):
-    prop = species_usage * species_concentration
-    prop = np.sum(prop, axis=1)
-    prop = prop * propensity_functions
-    return np.argmax(prop)
+    # # Calculamos las a_i
+    # a = species_usage * species_concentration
+    # # No se si hay que sumar multiplicar
+    # # a = np.sum(a, axis=1)
+    # a[a == 0] = 1
+    # a = np.prod(a, axis=1)
+    # a = a * propensity_functions
+
+    # a0 = np.sum(a)
+
+    a, a0 = calculate_a(propensity_functions, species_concentration, species_usage)
+    r2 = random.random()
+
+    # pdb.set_trace()
+
+    # Calculamos mu
+    mu = 1
+    while (np.sum(a[:mu]) < r2*a0) and (np.sum(a[:mu + 1]) >= r2*a0):
+        mu += 1
+
+    print("Encontrado mu", mu - 1)
+    # print(species_concentration)
+
+    return mu - 1
 
 
-def update_concentrations(reaction, species_concentration, propensity_functions):
+def next_time(propensity_functions, species_concentration, species_usage):
+    # a = species_usage * species_concentration
+    # a[a == 0] = 1
+    # a = np.prod(a, axis=1)
+    # a = a * propensity_functions
+
+    _, a0 = calculate_a(propensity_functions, species_concentration, species_usage)
+    r1 = random.random()
+
+    # Calculamos tau
+    tau = (1 / a0) * math.log(1 / r1)
+
+    return tau
+
+
+def update_concentrations(reaction, species_concentration, update_matrix):
     return np.add(species_concentration,
-                  propensity_functions[reaction])
+                  update_matrix[reaction])
 
 
 def gillespie(initial_time, final_time,
@@ -80,11 +131,11 @@ def gillespie(initial_time, final_time,
     # Iterar mientras que hay reactivos y no se ha pasado el tiempo de la simuación
     while t <= final_time and np.sum(species_concentration) > 0:
         reaction = next_reaction(propensity_functions, species_concentration, species_usage)
-        species_concentration = update_concentrations(reaction, species_concentration, propensity_functions)
+        species_concentration = update_concentrations(reaction, species_concentration, update_matrix)
 
         # incrementar el tiempo de forma aleatoria y siguiendo una
         # distribución uniforme
-        t += random.random()
+        t += next_time(propensity_functions, species_concentration, species_usage)
 
         times.append(t)
         concentrations.append(species_concentration)
